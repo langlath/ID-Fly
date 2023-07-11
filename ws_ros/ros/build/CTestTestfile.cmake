93b00b12c5c3c43edaaf2607bd46c203b4083dee
langlath <thibault.langlard@ensta-bretagne.org>
2023-07-11 09:15:40 +0200

    adding measures and their data

---
+++ ws_ros/ros/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/langlard/ros2_ws/src/novabot/ws_ros/ros/src
+# Build directory: /home/langlard/ros2_ws/src/novabot/ws_ros/ros/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("simu")
---
+++ ws_ros/ros/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/langlard/ros2_ws/src/novabot/ws_ros/ros/devel:/opt/ros/noetic:/home/langlard/Desktop/travail/ensta/libraries/rtac_asio'
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/home/langlard/Desktop/travail/ensta/libraries/rtac_asio/lib'
+export PWD='/home/langlard/ros2_ws/src/novabot/ws_ros/ros/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/langlard/ros2_ws/src/novabot/ws_ros/ros/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/langlard/ros2_ws/src/novabot/ws_ros/ros/src:$ROS_PACKAGE_PATH"
